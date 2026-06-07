0f99653f4919b9f1b4e4cae02a822a29d37b09d7
juhwankim-dev <juhwan.dev@gmail.com>
2026-06-08 00:57:45 +0900

    #6 온보딩 화면 연결

--- presentation/src/main/java/com/happyhouse/challa/presentation/navigation/ChallaNavHost.kt
+++ presentation/src/main/java/com/happyhouse/challa/presentation/navigation/ChallaNavHost.kt
@@ -3,6 +3,7 @@ package com.happyhouse.challa.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
+import com.happyhouse.challa.presentation.onboarding.OnboardingScreen
 import com.happyhouse.challa.presentation.sample.SampleScreen
 
 @Composable
@@ -13,6 +14,13 @@ fun ChallaNavHost(navigator: ChallaNavigator) {
             entryProvider {
                 entry<ChallaRoute.Sample> {
                     SampleScreen()
+                }
+                entry<ChallaRoute.Onboarding> {
+                    OnboardingScreen(
+                        onComplete = {
+                            // TODO JH: Login 화면 구현 후 navigator.replace(ChallaRoute.Login)
+                        },
+                    )
                 }
             },
     )
