7bd4d1fc6e232f7024fe22754028b9255b158db1
juhwankim-dev <juhwan.dev@gmail.com>
2026-06-18 01:08:42 +0900

    #7 홈 화면 연결

--- presentation/src/main/java/com/happyhouse/challa/presentation/navigation/ChallaNavHost.kt
+++ presentation/src/main/java/com/happyhouse/challa/presentation/navigation/ChallaNavHost.kt
@@ -3,6 +3,7 @@ package com.happyhouse.challa.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
+import com.happyhouse.challa.presentation.home.HomeScreen
 import com.happyhouse.challa.presentation.login.LoginScreen
 import com.happyhouse.challa.presentation.onboarding.OnboardingScreen
 import com.happyhouse.challa.presentation.sample.SampleScreen
@@ -26,7 +27,20 @@ fun ChallaNavHost(navigator: ChallaNavigator) {
                 entry<ChallaRoute.Login> {
                     LoginScreen(
                         onLoginSuccess = {
-                            // TODO JH: 로그인 성공 후 다음 화면(Home 등) 구현되면 navigator.replace(...) 연결
+                            navigator.replace(ChallaRoute.Home)
+                        },
+                    )
+                }
+                entry<ChallaRoute.Home> {
+                    HomeScreen(
+                        onNavigateToInviteCode = {
+                            // TODO JH: 초대 코드 입력 화면 구현되면 navigator.navigate(...) 연결
+                        },
+                        onNavigateToCreateRoom = {
+                            // TODO JH: 새 방 만들기 화면 구현되면 navigator.navigate(...) 연결
+                        },
+                        onNavigateToRoom = { _ ->
+                            // TODO JH: 방 상태별 화면(Gallery/Waiting/RoomMain) 구현되면 navigator.navigate(...) 연결
                         },
                     )
                 }
