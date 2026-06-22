b6dd24e5771e04949cfaaae42253de6866e69e3b
arinming <jyandysys1@naver.com>
2026-06-22 22:24:33 +0900

    #23 ChallaNavHost import 정렬 수정

--- presentation/src/main/java/com/happyhouse/challa/presentation/navigation/ChallaNavHost.kt
+++ presentation/src/main/java/com/happyhouse/challa/presentation/navigation/ChallaNavHost.kt
@@ -9,9 +9,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.ui.NavDisplay
 import com.happyhouse.challa.presentation.camera.CameraRoute
+import com.happyhouse.challa.presentation.gallery.GalleryRoute
 import com.happyhouse.challa.presentation.home.HomeScreen
 import com.happyhouse.challa.presentation.home.createroom.CreateRoomScreen
-import com.happyhouse.challa.presentation.gallery.GalleryRoute
 import com.happyhouse.challa.presentation.login.LoginScreen
 import com.happyhouse.challa.presentation.onboarding.OnboardingScreen
 import com.happyhouse.challa.presentation.sample.SampleScreen
