6a01eecfdb5f54785bc3642b572d8e74d0f2ffca
Ham BeomJoon <hbj0802@naver.com>
2026-06-24 22:58:15 +0900

    #32 디자인 시스템에 커스텀 폰트 추가 및 적용
    - SUIT 및 Dirtyline 폰트 파일 추가
    - `SuitFontFamily` 및 `DirtylineFontFamily` 정의
    - Typography의 `bodyLarge` 스타일에 `SuitFontFamily` 적용

--- presentation/src/main/java/com/happyhouse/challa/presentation/designsystem/theme/Type.kt
+++ presentation/src/main/java/com/happyhouse/challa/presentation/designsystem/theme/Type.kt
@@ -2,15 +2,35 @@ package com.happyhouse.challa.presentation.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.happyhouse.challa.presentation.R
+
+val SuitFontFamily =
+    FontFamily(
+        Font(R.font.suit_thin, FontWeight.Thin),
+        Font(R.font.suit_extra_light, FontWeight.ExtraLight),
+        Font(R.font.suit_light, FontWeight.Light),
+        Font(R.font.suit_regular, FontWeight.Normal),
+        Font(R.font.suit_medium, FontWeight.Medium),
+        Font(R.font.suit_semi_bold, FontWeight.SemiBold),
+        Font(R.font.suit_bold, FontWeight.Bold),
+        Font(R.font.suit_extra_bold, FontWeight.ExtraBold),
+        Font(R.font.suit_heavy, FontWeight.Black),
+    )
+
+val DirtylineFontFamily =
+    FontFamily(
+        Font(R.font.dirtyline, FontWeight.Normal),
+    )
 
 val Typography =
     Typography(
         bodyLarge =
             TextStyle(
-                fontFamily = FontFamily.Default,
+                fontFamily = SuitFontFamily,
                 fontWeight = FontWeight.Normal,
                 fontSize = 16.sp,
                 lineHeight = 24.sp,
