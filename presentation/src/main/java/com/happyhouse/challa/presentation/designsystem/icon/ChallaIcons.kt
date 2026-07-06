b0dc0b881acd06d3ab109c7f7bf0a02400bb046e
Ham BeomJoon <hbj0802@naver.com>
2026-07-06 19:26:47 +0900

    #32 디자인 시스템 아이콘 추가
    - ChallaIcons에 Check, Left, LightOff, LightOn, Refresh, right, UnfoldMore 아이콘 추가
    - ic_check.xml 벡터 드로어블 리소스 추가 및 디자인 시스템 연결

--- presentation/src/main/java/com/happyhouse/challa/presentation/designsystem/icon/ChallaIcons.kt
+++ presentation/src/main/java/com/happyhouse/challa/presentation/designsystem/icon/ChallaIcons.kt
@@ -14,11 +14,32 @@ object ChallaIcons {
     val Camera = R.drawable.ic_camera
 
     @DrawableRes
+    val Check = R.drawable.ic_check
+
+    @DrawableRes
     val Close = R.drawable.ic_close
 
     @DrawableRes
     val Kakao = R.drawable.ic_kakao
 
     @DrawableRes
+    val Left = R.drawable.ic_left
+
+    @DrawableRes
+    val LightOff = R.drawable.ic_light_off
+
+    @DrawableRes
+    val LightOn = R.drawable.ic_light_on
+
+    @DrawableRes
+    val Refresh = R.drawable.ic_refresh
+
+    @DrawableRes
+    val right = R.drawable.ic_right
+
+    @DrawableRes
     val Setting = R.drawable.ic_setting
+
+    @DrawableRes
+    val UnfoldMore = R.drawable.ic_unfold_more
 }
