9f223a69e099ab5575f9b9f165b2866ac2887ae7
juhwankim-dev <juhwan.dev@gmail.com>
2026-08-02 21:23:28 +0900

    #74 presigned URL 기반 프로필 이미지 업로드 구현

---
+++ domain/src/main/java/com/happyhouse/challa/domain/repository/ImageUploadRepository.kt
@@ -0,0 +1,15 @@
+package com.happyhouse.challa.domain.repository
+
+import com.happyhouse.challa.domain.result.ChallaResult
+
+interface ImageUploadRepository {
+    /**
+     * 프로필 이미지를 업로드하고, 저장에 사용할 공개 URL 을 돌려준다.
+     *
+     * 서버에서 S3 서명 URL 을 발급받아 클라이언트가 직접 S3 로 PUT 하는 방식이며,
+     * 성공 시 반환되는 URL 을 그대로 프로필의 `profileImageUrl` 로 저장하면 된다.
+     *
+     * @param imageUri 갤러리 등에서 선택한 이미지의 content URI 문자열
+     */
+    suspend fun uploadProfileImage(imageUri: String): ChallaResult<String>
+}
