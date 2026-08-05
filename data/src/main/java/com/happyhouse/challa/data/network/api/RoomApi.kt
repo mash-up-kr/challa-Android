c634c025380f0d78dca35e9423a437819eb523d1
juhwankim-dev <juhwan.dev@gmail.com>
2026-08-05 23:29:14 +0900

    #76 방 입장 API 구현

--- data/src/main/java/com/happyhouse/challa/data/network/api/RoomApi.kt
+++ data/src/main/java/com/happyhouse/challa/data/network/api/RoomApi.kt
@@ -3,6 +3,8 @@ package com.happyhouse.challa.data.network.api
 import com.happyhouse.challa.data.network.dto.BaseResponse
 import com.happyhouse.challa.data.network.dto.CreateRoomRequest
 import com.happyhouse.challa.data.network.dto.CreateRoomResponse
+import com.happyhouse.challa.data.network.dto.JoinRoomRequest
+import com.happyhouse.challa.data.network.dto.JoinRoomResponse
 import com.happyhouse.challa.domain.result.ChallaResult
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -12,4 +14,9 @@ interface RoomApi {
     suspend fun postRoom(
         @Body request: CreateRoomRequest,
     ): ChallaResult<BaseResponse<CreateRoomResponse>>
+
+    @POST("api/v1/rooms/join")
+    suspend fun joinRoom(
+        @Body request: JoinRoomRequest,
+    ): ChallaResult<BaseResponse<JoinRoomResponse>>
 }
