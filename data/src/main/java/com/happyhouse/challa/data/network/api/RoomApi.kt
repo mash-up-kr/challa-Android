51390b9bc20786c0b8d2822067c5173745d89293
juhwankim-dev <juhwan.dev@gmail.com>
2026-08-08 22:18:24 +0900

    ktlint 적용

--- data/src/main/java/com/happyhouse/challa/data/network/api/RoomApi.kt
+++ data/src/main/java/com/happyhouse/challa/data/network/api/RoomApi.kt
@@ -3,9 +3,9 @@ package com.happyhouse.challa.data.network.api
 import com.happyhouse.challa.data.network.dto.BaseResponse
 import com.happyhouse.challa.data.network.dto.CreateRoomRequest
 import com.happyhouse.challa.data.network.dto.CreateRoomResponse
+import com.happyhouse.challa.data.network.dto.GetRoomsResponse
 import com.happyhouse.challa.data.network.dto.JoinRoomRequest
 import com.happyhouse.challa.data.network.dto.JoinRoomResponse
-import com.happyhouse.challa.data.network.dto.GetRoomsResponse
 import com.happyhouse.challa.domain.result.ChallaResult
 import retrofit2.http.Body
 import retrofit2.http.GET
