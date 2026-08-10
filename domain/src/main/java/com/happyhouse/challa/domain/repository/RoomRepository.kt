89f0284975684c21c8891a453cf81a0bf2fdf9ed
juhwankim-dev <juhwan.dev@gmail.com>
2026-08-11 00:08:58 +0900

    ktlint 적용

--- domain/src/main/java/com/happyhouse/challa/domain/repository/RoomRepository.kt
+++ domain/src/main/java/com/happyhouse/challa/domain/repository/RoomRepository.kt
@@ -1,10 +1,9 @@
 package com.happyhouse.challa.domain.repository
 
 import com.happyhouse.challa.domain.model.CreatedRoom
-import com.happyhouse.challa.domain.model.ShootableRoom
 import com.happyhouse.challa.domain.model.Room
 import com.happyhouse.challa.domain.model.RoomStatus
-import com.happyhouse.challa.domain.model.RoomSummary
+import com.happyhouse.challa.domain.model.ShootableRoom
 import com.happyhouse.challa.domain.result.ChallaResult
 
 interface RoomRepository {
