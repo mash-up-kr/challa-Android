891777604a1482804914a19c4b125603042dbb70
juhwankim-dev <juhwan.dev@gmail.com>
2026-08-27 22:50:46 +0900

    #95 설정 아이콘을 컴포넌트 사용하지 않고 직접 구현
    왜?
    컴포넌트랑 사이즈가 달라서

--- presentation/src/main/java/com/happyhouse/challa/presentation/gallery/component/GalleryTopBar.kt
+++ presentation/src/main/java/com/happyhouse/challa/presentation/gallery/component/GalleryTopBar.kt
@@ -1,9 +1,16 @@
 package com.happyhouse.challa.presentation.gallery.component
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.PreviewWrapper
+import androidx.compose.ui.unit.dp
 import com.happyhouse.challa.presentation.R
 import com.happyhouse.challa.presentation.designsystem.component.ChallaTopNavigation
 import com.happyhouse.challa.presentation.designsystem.component.ChallaTopNavigationVariant
@@ -12,6 +19,8 @@ import com.happyhouse.challa.presentation.designsystem.component.button.ChallaBu
 import com.happyhouse.challa.presentation.designsystem.component.button.ChallaIconButton
 import com.happyhouse.challa.presentation.designsystem.icon.ChallaIcons
 import com.happyhouse.challa.presentation.designsystem.preview.ChallaPreviewWrapper
+import com.happyhouse.challa.presentation.designsystem.theme.ChallaTheme
+import com.happyhouse.challa.presentation.designsystem.util.noRippleClickOnce
 import androidx.compose.ui.tooling.preview.Preview as ComposePreview
 
 /**
@@ -39,13 +48,20 @@ fun GalleryTopBar(
             )
         },
         trailingIcon = {
-            ChallaIconButton(
-                icon = ChallaIcons.Setting,
-                onClick = onSettingClick,
-                contentDescription = stringResource(R.string.gallery_setting_description),
-                variant = ChallaButtonVariant.TRANSPARENT,
-                size = ChallaButtonSize.MEDIUM,
-            )
+            Box(
+                modifier =
+                    modifier
+                        .size(40.dp)
+                        .noRippleClickOnce(role = Role.Button, onClick = onSettingClick),
+                contentAlignment = Alignment.Center,
+            ) {
+                Icon(
+                    painter = painterResource(id = ChallaIcons.Setting),
+                    contentDescription = stringResource(R.string.gallery_setting_description),
+                    modifier = Modifier.size(24.dp),
+                    tint = ChallaTheme.colors.labelNeutral,
+                )
+            }
         },
     )
 }
