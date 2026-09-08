83d5b9e6d44af17fa35edf709913d01e874af317
Ham BeomJoon <hbj0802@naver.com>
2026-09-08 22:31:04 +0900

    #134 갤러리 하단 액션 바 레이아웃 제약 수정
    - GalleryBottomActionHeight를 고정 높이 대신 최소 높이(heightIn)로 설정하도록 변경

--- presentation/src/main/java/com/happyhouse/challa/presentation/gallery/GalleryScreen.kt
+++ presentation/src/main/java/com/happyhouse/challa/presentation/gallery/GalleryScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
@@ -160,7 +161,7 @@ private fun GalleryScaffold(
                     Modifier
                         .align(Alignment.BottomCenter)
                         .fillMaxWidth()
-                        .height(GalleryBottomActionHeight)
+                        .heightIn(GalleryBottomActionHeight)
                         .then(measureBottomBar)
                         .then(bottomActionBackground),
                 contentAlignment = Alignment.BottomCenter,
