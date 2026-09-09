101eed789551afd89626eb7cc58935a36e070f55
Ham BeomJoon <hbj0802@naver.com>
2026-09-09 12:11:15 +0900

    #139 릴리스 빌드 최적화 및 리소스 축소 활성화
    - release 빌드 타입에서 isMinifyEnabled를 true로 변경
    - isShrinkResources 설정을 true로 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,8 @@ android {
         }
 
         release {
-            isMinifyEnabled = false
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
